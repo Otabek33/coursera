5826977ae8317dd71dde5ec51995c8d8250b6e49
Otabek <89254293+Otabek33@users.noreply.github.com>
2023-09-02 15:21:59 +0500

    Update simple_interest.sh

--- simple_interest.sh
+++ simple_interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    Author: Upkar Lidder (IBM)
    Additional Authors:
-   Otabek33
+  * **[Otabek Davronov]( https://github.com/Otabek33)**
    # Input:
    # p, principal amount
    # t, time period in years
